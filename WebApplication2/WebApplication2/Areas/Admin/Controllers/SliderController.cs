93e2eca07aae1cf8ddc82eaa63ea3db8eb9d290b
User <aliyahyahs@code.edu.az>
2024-02-29 14:54:27 +0400

    go

--- WebApplication2/WebApplication2/Areas/Admin/Controllers/SliderController.cs
+++ WebApplication2/WebApplication2/Areas/Admin/Controllers/SliderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Contexts;
+using WebApplication2.Models;
 using WebApplication2.ViewModels;
 
 namespace WebApplication2.Areas.Admin.Controllers;
@@ -24,6 +25,19 @@ public class SliderController : Controller
             Shippings = Shippings
         };
         return View(viewModel);
+    }
+    public IActionResult Create()
+    {
+        return View();
+
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(Slider slider)
+    {
+        await _context.Sliders.AddAsync(slider);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+
     }
 }
 
